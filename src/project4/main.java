37b7461029022968602fcbe9173c6aac9e8f84d3
pc660 <pc660@hotmail.com>
2014-07-31 19:45:21 -0400

    1

--- src/project4/main.java
+++ src/project4/main.java
@@ -4,8 +4,15 @@ public class main {
 	public static void main(String[] args) 
 	{
 		MPI.Init(args);
-		
-		
-		
+		int num = 2;
+		if (args.length > 0) {
+			num = Integer.parseInt(args[0]);
+		}
+		String input_point = "./input/cluster.csv";
+		String output_point = "./output_point.csv";
+		clusterPoint tmp = new clusterPoint();
+		tmp.readCSV(input_point);
+		tmp.start();
+		MPI.Finalize(); 
 	}
 }
