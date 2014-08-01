9d0ab4c2f0cac3fb2347691e667592de15f396b0
pc660 <pc660@hotmail.com>
2014-07-31 20:26:07 -0400

    1

--- src/project4/clusterDNA.java
+++ src/project4/clusterDNA.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
-
+import mpi.*;
 public class clusterDNA {
 	
 	public String [] centers;
